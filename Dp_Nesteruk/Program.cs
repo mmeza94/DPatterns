519b3069552ed68b942f64a0ee1c619a521a1e88
Manuel Meza <mmezaram@emeal.nttdata.com>
2023-12-04 21:20:51 -0600

    changes

--- Dp_Nesteruk/Program.cs
+++ Dp_Nesteruk/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 
+// using AbstractFactory;
+using AbstractFactoryOCP;
 using Factory.FactoryMethod;
 using Factory.FactoryMethodAsync;
 using Factory.ThemeFactory;
@@ -28,36 +30,39 @@ using System;
 
 #region TrackingThemeFactory
 
-var factory = new TrackingThemeFactory();
-var darkTheme = factory.CreateTheme(true);
-var light = factory.CreateTheme(false);
-Console.WriteLine(factory.Info);
+// var factory = new TrackingThemeFactory();
+// var darkTheme = factory.CreateTheme(true);
+// var light = factory.CreateTheme(false);
+// Console.WriteLine(factory.Info);
 
-var factory2 = new ReplaceableThemFactory();
-var magictheme = factory2.CreateTheme(true);
-Console.WriteLine(magictheme.value.BgrColor);
-factory2.ReplaceTheme(false);
-Console.WriteLine(magictheme.value.BgrColor);
+// var factory2 = new ReplaceableThemFactory();
+// var magictheme = factory2.CreateTheme(true);
+// Console.WriteLine(magictheme.value.BgrColor);
+// factory2.ReplaceTheme(false);
+// Console.WriteLine(magictheme.value.BgrColor);
 
 
 #endregion
 
 
 
+#region Abstract Factory
+
+// var machine = new HotDrinkMachine();
+// var coffee = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Coffee,60);
+// coffee.Consume();
+
+#endregion
 
 
 
+#region Abstract Factory OCP
+
+var machine = new AbstractFactoryOCP.HotDrinkMachine();
+var drink = machine.MakeHotDrink();
 
 
-
-
-
-
-
-
-
-
-
+#endregion
 
 
 
---
+++ Dp_Nesteruk/Readme.md
@@ -0,0 +1,6 @@
+Factory method
+A static method that creates objects
+
+Factory
+A separate class that takes care of the creation of a certain object
+May be external or inner class
